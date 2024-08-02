a884e1667a935c65056882d994a43ed0b080349a
inevitable <inevitable@Panther.localdomain>
2024-08-02 22:55:02 +0530

    Backend started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import About from "./pages/About";
 const App = () => {
-  return <div className="text-3xl text-red-800">app</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
